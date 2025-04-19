85b897bd71dfae8b69887d9d6f4b1bf6f56183d4
mwagenipecy <percyegno@gmail.com>
2025-04-19 19:53:05 +0300

    new changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,7 +36,7 @@
                 </nav>
 
                      <!-- Language Toggle Button -->
-                     <div class="relative" x-data="{ open: false, language: '{{ app()->getLocale() }}' }">
+                     <div class="relative" x-data="{ open: false, language: '{{ session('locale') }}' }">
                     <button @click="open = !open" class="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 px-3 py-2 rounded-md transition">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -49,7 +49,7 @@
                     </div>
                 </div>
 
-                
+
             </div>
         </div>
     </div>
