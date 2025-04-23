78af728a6f63d44a729147f124a26a545f45d38a
mwagenipecy <percyegno@gmail.com>
2025-04-24 01:22:27 +0300

    done

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Collection Project</title>
+    <title>kamatimadini</title>
     <link rel="icon" href="{{ asset('/image/courtOfArm1.png') }}" type="image/png">
 
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
